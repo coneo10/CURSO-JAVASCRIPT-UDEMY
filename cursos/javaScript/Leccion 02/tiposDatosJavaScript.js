94cdd957c9824f203eb6cca58670d20ad7f92891
coneo <coneodiaz25@gmail.com>
2023-01-28 16:39:21 -0500

    Tipos de datos en JavaScript

--- cursos/javaScript/Leccion 02/tiposDatosJavaScript.js
+++ cursos/javaScript/Leccion 02/tiposDatosJavaScript.js
@@ -1,91 +1,37 @@
+
+// concatenacion de cadenas
+var nombre = 'juan';
+var apellido = 'coneo';
+
+var nombreCompleto= nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+// otra forma de concatenar
+
+var nombreCompleto2 = 'carlos' + ' ' + 'coneo';
+console.log(nombreCompleto2);
+
+//concatenar numero a cadena
+
+var edadJuan = nombre + 26;
+console.log(edadJuan);
+
 /*
-Ejemplo de tipos de datos en
-javaScript */
-
-//Tipo de dato string 
-
-var nombre="carlos";
-console.log(nombre);
-
-//Tipo de dato numerico 
-var numero="26";
-console.log(numero);
-
-
-//tipo de dato object
-var objeto={
-    nombre: "Sara", 
-    apellido:"Lopez",
-    telefono:"3017913870"
-}
-
-console.log(objeto);
-
-
-// variable dinamica asignando valor a variable previamente definida
-
-nombre=10;
-
-//operador typeof para mostrar de que tipo es la variable
-console.log(typeof nombre);
-
-console.log(typeof objeto); 
-
-console.log()
-
-
-
-// tipo de dato bolean (true, false)
-
-var bandera = true;
-console.log(bandera);
-console.log(typeof bandera);
-
-//tipo de dato function
-function miFuncion() {}
-console.log(miFuncion);
-console.log(typeof miFuncion)
-
-// tipo de dato symbol
-
-var simbolo = Symbol();
-var miSimbolo = Symbol('tipo cadena');
-console.log(typeof miSimbolo)
-
-// tipo clase es una funcion tambien 
-
-class persona{
-    constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-}
-console.log(persona);
-console.log(typeof persona);
-
-// tipo de dato undefined
-
-var x;
-console.log(typeof x);
-
-/* undefined tambien 
-como valor a la variable
+aqui vemos que no se realiza una suma de los numeros
+si no que se concatenan los valores 26 y 4 
+convirtiendolos a una cadena
 */
-x = undefined;
-console.log(typeof x);
+var edadJuan = nombre +26 + 4;
+console.log(edadJuan)
 
-// palabra null= ausencia de valor
-var y =null;
-console.log(typeof y);
+// caso si se quisiera suma
 
+var suma = nombre + (26+4);
+console.log(suma); 
 
-//ARREGLOS O ARRAY 'Son de tipo object"
-
-var autos = ['BMW', 'Audi', 'TOYOTA'];
-console.log(autos);
-
-console.log(typeof autos);
-
-var x = '';
-console.log(x);
-console.log(typeof x); 
+/*
+otra forma de demostrar el orden de como
+javaScript evalua las expresiones de izq a derecha
+*/
+suma = 2+4 + nombre;
+console.log(suma);
