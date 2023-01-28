d1b4c6a3f8da46500ec2633e1ded21278c675c2a
coneo <coneodiaz25@gmail.com>
2023-01-28 16:42:13 -0500

    Uso basico de variables javaScript

---
+++ cursos/javaScript/Leccion03/usoBasicoVariables.js
@@ -0,0 +1,32 @@
+
+// declaracion sin variable reservada
+
+nombre = 'juan';
+console.log(nombre);
+
+
+// declaracion de variable con let
+
+let nombre2 = 'carlos';
+console.log(nombre2);
+
+
+//declarar variable sin asignar valor
+
+let nombre3;
+nombre3='juan';
+console.log(nombre3);
+
+
+
+//declaracion variable CONST
+
+/*
+No se puede modificar una variable
+definida con const una ves ya se 
+le haya asignado un valor
+*/
+const apellido = 'perez';
+apellido ='lara';
+
+
