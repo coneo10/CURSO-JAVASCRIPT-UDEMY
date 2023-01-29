40d0f1c0d0b7853e96e4660a005e1693ce06104e
coneo <coneodiaz25@gmail.com>
2023-01-28 23:47:00 -0500

    OperadoresJavaScript

---
+++ cursos/javaScript/Leccion04/operadoresJavaScript.js
@@ -0,0 +1,49 @@
+
+//operador suma + (addition)
+let a = 3, b= 2;
+let c = a+b;
+console.log("resultado de la suma: " + c);
+
+
+//operador resta (subtraction)
+
+let d =3 , e= 2;
+let f = d-e;
+console.log("resultado de la resta:" + f);
+
+// Operador multiplicador
+
+let multiplicador = 3, multiplicador2 =5;
+
+let resultadoMultiplicacion = multiplicador * multiplicador2;
+console.log(resultadoMultiplicacion);
+
+
+//operador division
+let dividir = 20 / 5;
+console.log(dividir);
+
+//otra forma
+
+let division =20, division2=5;
+let resultadoDivision= (division / division2);
+console.log(resultadoDivision);
+
+//operador modulus (residuo de la division)
+
+let modulo = 3, modulo2 =2;
+let $moduloResultado= modulo % modulo2;
+console.log($moduloResultado);
+
+//otra forma 
+modulo = 3%2;
+console.log(modulo);
+
+// operador exponente multiplica el elevado que seria 3 a la 2= 9
+exponente = 3 ** 2;
+console.log(exponente);
+
+//otra forma exponencial
+let expo = 5 , expo2 = 2;
+let _expoResultado= expo**expo2;
+console.log(_expoResultado);
