6afde6bdf43108d08f14c3977d8bec14b7054440
coneo <coneodiaz25@gmail.com>
2023-01-28 21:55:22 -0500

    Buenas practicas y de variables y definir nombres con variables

---
+++ cursos/javaScript/Leccion03/buenasPracticasVariables.js
@@ -0,0 +1,17 @@
+
+
+//Notacion camello en la variable inicial minuscula, sgt palabra con inicial Mayuscula
+
+// sintaxis correcta para declarar variables 
+let nombreCompleto = 'juan perez';
+console.log(nombreCompleto);
+
+// Variables juntas en una misma linea
+
+let x, y;
+x = 10, y = 20;
+
+// tercera variable teniendo la suma de los anteriores
+
+let z = x+y ;
+console.log(z);
---
+++ cursos/javaScript/Leccion03/reglasDefinirNombresVariables.js
@@ -0,0 +1,37 @@
+
+//Js Sensible a minusculas y Mayusculas
+let nombreCompleto = "Carlos Coneo";
+console.log(nombreCompleto);
+
+// minuscula
+let nombrecompleto = 'carlos diaz ';
+console.log(nombrecompleto);
+
+// variable no definida porque se declararon con n minuscula y se escribieron con N mayuscula
+console.log(Nombrecompleto);
+
+
+/*
+En conclusion, se debe tener en cuenta como 
+escribimos o definimos la variable y asi
+mismo pasar el argumento tal cual como fue definida.
+*/
+
+
+// error al tratar de definir una variable comenzando por un numero.
+//no esta permitido iniciar asi 
+let 1nombreCompleto; 
+
+// caracteres permitidos
+let _nombreCompleto; //guion
+let bNombrecompleto; //letra Mayus o Minus
+let $nombreCompleto; // signo de dolar
+
+//forma permitida con numeros
+
+let _1nombreCompleto;
+let $2nombrecompleto;
+
+//no se usan las palabras reservadas como nombres de variables
+
+let break =10;
